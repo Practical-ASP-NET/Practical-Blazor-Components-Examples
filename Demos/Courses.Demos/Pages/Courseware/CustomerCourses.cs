5f69106366a793b205125ae63190ea007c2a7450
Jon Hilton <jon@jonhilton.net>
2023-03-06 22:41:03 +0000

    Added advanced panels demo

---
+++ Demos/Courses.Demos/Pages/Courseware/CustomerCourses.cs
@@ -0,0 +1,12 @@
+﻿namespace Courses.Demos.Pages.Courseware;
+
+public class CustomerCourses
+{
+    public IEnumerable<CustomerCourseDetails?> Owns { get; set; }
+}
+
+public class CustomerCourseDetails {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Purchased { get; set; }
+}
--- Demos/Courses.Demos/Program.cs
+++ Demos/Courses.Demos/Program.cs
@@ -2,6 +2,7 @@ using Courses.Demo.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Courses.Demos;
+using Courses.Demos.Pages.Courseware;
 using Refit;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,6 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<CustomerCoursesStore>();
 
 var apiUrl = builder.HostEnvironment.BaseAddress;
 
