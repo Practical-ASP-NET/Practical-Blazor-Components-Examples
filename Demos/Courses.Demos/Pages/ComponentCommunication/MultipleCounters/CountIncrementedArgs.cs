5ddb2e17ba14e6a2b362d91ff22bb0abbebeba06
Jon <hilton.jon@gmail.com>
2023-05-17 11:36:22 +0100

    added multiple counters exercise

---
+++ Demos/Courses.Demos/Pages/ComponentCommunication/MultipleCounters/CountIncrementedArgs.cs
@@ -0,0 +1,7 @@
+﻿namespace Courses.Demos.Pages.ComponentCommunication.MultipleCounters;
+
+public class CountIncrementedArgs
+{
+    public int NewCount { get; set; }
+    public int IncrementedBy { get; set; }
+}
