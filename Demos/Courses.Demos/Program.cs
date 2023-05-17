3c94645b1e74e150943c6da8a5692a52dbf51e90
Jon <hilton.jon@gmail.com>
2023-05-17 11:27:53 +0100

    Reorganising things slightly

--- Demos/Courses.Demos/Program.cs
+++ Demos/Courses.Demos/Program.cs
@@ -2,7 +2,6 @@ using Courses.Demo.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Courses.Demos;
-using Courses.Demos.Pages.Courseware;
 using Refit;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,7 +9,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<CustomerCoursesStore>();
 
 var apiUrl = builder.HostEnvironment.BaseAddress;
 
